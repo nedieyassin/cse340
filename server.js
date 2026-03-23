b4225c9467483e6a2e30b6adab5f76676fec0c91
Nedie Yassin <yassinnedie9121@gmail.com>
2026-03-23 23:06:17 +0200

    w3

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
@@ -19,14 +21,12 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
